1d12a037d94e2280276f437efc2e6eac8a3119e1
David <investvirt@gmail.com>
2021-09-15 12:48:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+        $('.slider__inner').slick({
+                draggable: false,
+                focusOnSelect: false,
+                nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+                prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+                infinite: false
+        })
+
+
+})
